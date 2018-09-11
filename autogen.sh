1cd162992625d23d670a19e3ec572026b7ea919d
Nathan Goldstick <goldsticknt@goldsticknt.com>
2018-09-11 13:16:12 +0000

    Ylwrap is installed by autogen and should be removed by autogen.

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 
 if [ x$1 = x-c ]; then
 	rm -rf autom4te.cache m4
-	rm -f config.h.in configure compile depcomp install-sh ltmain.sh missing aclocal.m4 Makefile.in INSTALL ar-lib config.guess config.sub
+	rm -f config.h.in configure compile depcomp install-sh ltmain.sh missing aclocal.m4 Makefile.in INSTALL ar-lib config.guess config.sub ylwrap
 else
 	autoheader
 	aclocal
