ae73864d9f143f6a7bdf67d8a57af302dc4a2202
Nathan Goldstick <goldsticknt@goldsticknt.com>
2018-09-20 15:33:13 +0000

    Add the ability to call splint using make check.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [ x$1 = x-c ]; then
-	rm -rf autom4te.cache m4
-	rm -f config.h.in configure compile depcomp install-sh ltmain.sh missing aclocal.m4 Makefile.in INSTALL ar-lib config.guess config.sub ylwrap
+	rm -rf autom4te.cache || /bin/true
+	rm -f m4/libtool.m4 m4/lt~obsolete.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 || /bin/true
+	rmdir m4 || /bin/true
+	rm -f config.h.in configure compile depcomp install-sh ltmain.sh missing aclocal.m4 Makefile.in INSTALL ar-lib config.guess config.sub ylwrap test-driver || /bin/true
+	rm -f aminclude_static.am || /bin/true
 else
 	autoheader
 	aclocal
