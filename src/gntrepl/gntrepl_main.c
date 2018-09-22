4c31a9b92b68eca563bd30cdbc9fbb63c5c24fb8
Nathan Goldstick <goldsticknt@goldsticknt.com>
2018-09-22 06:13:55 +0000

    Rearrange code so that splint passes. Need to reenable checks/specify functionality.

--- src/gntrepl/gntrepl_main.c
+++ src/gntrepl/gntrepl_main.c
@@ -1,16 +1,22 @@
+#define GNTREPL_MAIN_C
+
 #include <stdio.h>
 
+#ifndef S_SPLINT_S
 #include <gntlisp.h>
+#endif /* S_SPLINT_S */
 
 #include "maindefs.h"
 
 #include "config.h"
 
-int main(int argc, char *argv[])
+int main(/*@unused@*/ int argc, /*@unused@*/ char *argv[])
 {
+#ifndef S_SPLINT_S
 	printf("%s: %d\n", PACKAGE_NAME, gntlisp_get_version());
 	//gntlisp_scan();
 	gntlisp_parse();
+#endif /* S_SPLINT_S */
 	return(0);
 }
 
